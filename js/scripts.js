1ffc7c99e2ffc70437d2a6f28673470d62f3932e
glenbuck503 <glenbuck503@gmail.com>
2021-01-22 09:12:08 -0800

    add const to hold parsed input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-function rogers(userNumber) {
+function rogers(inputNumber) {
   const numArray = []
 }
+
+
+
+
 
 
 $(document).ready(function() {
   $("form#rogers").submit(function() {
     event.preventDefault();
+
+    let inputNumber = parseInt($("input#inputNumber").val());
+    console.log(inputNumber)
+
+
+
 
 
   });
