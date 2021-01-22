01f0eaab2fdca51e4bd9138cd307b6ccb0204f83
glenbuck503 <glenbuck503@gmail.com>
2021-01-22 15:43:09 -0800

    add user name to the reversed function

--- js/scripts.js
+++ js/scripts.js
@@ -21,10 +21,11 @@ function rogers(inputNumber) {
 
 function rogersReverse(inputNumber) {
   const numArray = [];
+  let inputName = $("input#name").val();
   for (let i = 0; i <= inputNumber; i++) {
     let number = i.toString();
     if (number.includes(3)) {
-      numArray.push("Won't you be my neighbor?" + ", ")
+      numArray.push("Won't you be my neighbor, " + inputName + "?" + ", ")
     } else if (number.includes(2)) {
       numArray.push("Boop!" + ", ")
     } else if (number.includes(1)) {
